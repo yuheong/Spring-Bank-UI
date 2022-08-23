b1d9971d4dbcb5fc5aa51980dce035bcad28af02
Yuhe Ong <yuhe_ong@hotmail.com>
2022-08-23 14:04:43 +0800

    fix table width and add loading indicator

--- src/ManageApplicants.js
+++ src/ManageApplicants.js
@@ -1,36 +1,44 @@
+import { Spin } from "antd";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import api from "./api";
 import "./ManageApplicants.css";
 
 export default function ManageApplicants(props) {
+  const [isLoading, setIsLoading] = useState(true);
   const [customers, setCustomers] = useState([]);
 
   useEffect(() => {
     api.listCustomers().then((res) => {
       setCustomers(res);
+      setIsLoading(false);
     });
   }, []);
 
   return (
     <>
       <h1>All Applicants</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Applicant Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          {customers.map((customer, id) => (
-            <tr key={id}>
-              <td>
-                <a href={`/applicants/${customer.id}`}>{customer.name}</a>
-              </td>
+      {isLoading ? (
+        <Spin size="large" />
+      ) : (
+        <table className="manage-applicants">
+          <thead>
+            <tr>
+              <th>Applicant Name</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {customers.map((customer, id) => (
+              <tr key={id}>
+                <td>
+                  <a href={`/applicants/${customer.id}`}>{customer.name}</a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+
       <div
         style={{
           display: "flex",
